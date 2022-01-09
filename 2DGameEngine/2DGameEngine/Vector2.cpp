f94f6dc843518e756e520936b75c9fd62bc83d9e
Christian Julch <ctjulch@student.fullsail.edu>
2022-01-09 10:36:32 -0500

    added: methods and properties to vector2 files.

---
+++ 2DGameEngine/2DGameEngine/Vector2.cpp
@@ -0,0 +1,65 @@
+#include "stdafx.h"
+#include "Vector2.h"
+
+Vector2 Vector2::down()
+{
+    return Vector2();
+}
+
+Vector2 Vector2::up()
+{
+    return Vector2();
+}
+
+Vector2 Vector2::right()
+{
+    return Vector2();
+}
+
+Vector2 Vector2::left()
+{
+    return Vector2();
+}
+
+Vector2 Vector2::one()
+{
+    return Vector2();
+}
+
+Vector2 Vector2::zero()
+{
+    return Vector2();
+}
+
+float Vector2::magnitude()
+{
+    return 0.0f;
+}
+
+Vector2 Vector2::normalized()
+{
+    return Vector2();
+}
+
+float Vector2::x()
+{
+    return 0.0f;
+}
+
+float Vector2::y()
+{
+    return 0.0f;
+}
+
+bool Vector2::equals(Vector2 _vec)
+{
+    return false;
+}
+
+void Vector2::normalize()
+{
+}
+
+void Vector2::set(float _x, float _y)
+{
+}
--- 2DGameEngine/2DGameEngine/Vector2.h
+++ 2DGameEngine/2DGameEngine/Vector2.h
@@ -6,5 +6,25 @@ private:
 	float xValue;
 	float yValue;
 
+public:
+	// static
+	Vector2 down();
+	Vector2 up();
+	Vector2 right();
+	Vector2 left();
+	Vector2 one();
+	Vector2 zero();
+
+	// properties
+	float magnitude();
+	Vector2 normalized();
+	float x();
+	float y();
+
+	// public methods
+	bool equals(Vector2 _vec);
+	void normalize();
+	void set(float _x, float _y);
+
 };
 
