1b2d815665b716663410f6989dd998750adf9f89
Christian Julch <ctjulch@student.fullsail.edu>
2022-01-09 09:36:07 -0500

    added game.cpp and functionallity

--- 2DGameEngine/2DGameEngine/Game.cpp
+++ 2DGameEngine/2DGameEngine/Game.cpp
@@ -1,2 +1,54 @@
 #include "stdafx.h"
 #include "Game.h"
+
+Game::Game(int _width, int _height)
+{
+	width = _width;
+	height = _height;
+
+	initWindow(_width, _height);
+}
+
+Game::~Game() {}
+
+void Game::inputs()
+{
+	// Poll window events
+	while (window.isOpen())
+	{
+		// If the user presses the exit button on the application.
+		if (ev.type == sf::Event::Closed) {
+			//debug.log("Application closing...");
+			window.close();
+		}
+		else if (ev.type == sf::Event::KeyPressed && ev.key.code == sf::Keyboard::Escape) {
+			//debug.log("Application closing...");
+			window.close();
+		}
+	}
+}
+
+void Game::update()
+{
+	// Run inputs before updating.
+	inputs();
+}
+
+void Game::render()
+{
+	//window.clear(sf::Color::Black);
+
+	// Render here ->
+
+	//window.display();
+}
+
+const sf::RenderWindow& Game::getWindow() const
+{
+	return window;
+}
+
+void Game::initWindow(int _width, int _height)
+{
+	window.create(sf::VideoMode(_width, _height), "SFML Project", sf::Style::Close | sf::Style::Titlebar);
+}
--- 2DGameEngine/2DGameEngine/Game.h
+++ 2DGameEngine/2DGameEngine/Game.h
@@ -1,5 +1,21 @@
-#pragma once
 class Game
 {
+	int width, height;
+	sf::RenderWindow window;
+
+	sf::Event ev;
+
+public:
+	Game(int _width, int _height);
+	virtual ~Game();
+
+	void inputs();
+	void update();
+	void render();
+
+	const sf::RenderWindow& getWindow() const;
+
+private:
+	void initWindow(int _width, int _height);
 };
 
